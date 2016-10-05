7b5f131ee24f91c252454961622961f282a3a593
Kaustabh04 <kaustabh04@gmail.com>
2016-10-06 00:25:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// Variables declared
+
+  var sum, i;
+  var sum = 0;
+  
+// For loop 
+
+  for(i=0; i<=1000; i++){
+    sum += i;
+  }
+
+// Print the sum in the console  
+
+  console.log("Calculating the sum by For loop : " + sum);
+ 
+// While loop
+
+  while(i<=1000){
+	  sum += i;
+	i++
+  }
+
+// Print the sum in the console
+  
+  console.log("Calculating the sum by While loop : " + sum);
+ 
